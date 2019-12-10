6775734731eea52ddb37a8cf158e84f1898853de
BMWeygant <bobbydigital108@gmail.com>
2019-12-10 11:26:16 -0500

    Followed internet tutorial to create first test(sum.js) & solution(index.js).

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
-const makeTest = (a, b) => {
-    return a * b
+//Sample code from internet tutorial
+module.exports = function() {
+
+    // Convert arguments object to an array
+    var args = Array.prototype.slice.call(arguments);
+    
+    // Throw error if arguments contain non-finite number values
+    if (!args.every(Number.isFinite)) {
+      throw new TypeError('sum() expects only numbers.')
+    }
+    
+    // Return the sum of the arguments
+    return args.reduce(function(a, b) {
+      return a + b
+    }, 0);
 }
