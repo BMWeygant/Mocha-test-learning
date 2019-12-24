64c79e12fcaf412fe6ab790468253c41d5e0df42
BMWeygant <bobbydigital108@gmail.com>
2019-12-24 15:00:23 -0500

    removed comments from index

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
-//Simplified test solutions
-
 const sum = (a, b) => {
     return a + b
     
 }
-
-//Added own sub test, solution below
 
 const sub = (a, b) => {
     return a - b 
@@ -22,20 +18,3 @@ const modernArt = {
 
 
 module.exports = {sum, sub, modernArt}
-//Sample code from internet tutorial
-// module.exports = function() {
-
-//     // Convert arguments object to an array
-//     var args = Array.prototype.slice.call(arguments);
-    
-//     // Throw error if arguments contain non-finite number values
-//     if (!args.every(Number.isFinite)) {
-//       throw new TypeError('sum() expects only numbers.')
-//     }
-    
-//     // Return the sum of the arguments
-//     return args.reduce(function(a, b) {
-//       return a + b
-//     }, 0);
-// }
-//All tests related to sum pass
