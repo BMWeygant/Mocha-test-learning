a025e9b0edc99d0257c0fe4b93878824881794a3
BMWeygant <bobbydigital108@gmail.com>
2019-12-24 15:02:38 -0500

    Removed clutter of comments from test file.

--- test/sum.js
+++ test/sum.js
@@ -1,6 +1,3 @@
-//Sample from tutorial
-//Heavily modified to be more beginner user friendly
-
 const {sum, sub, modernArt} = require('../index');
 var expect = require('chai').expect;
 
@@ -8,37 +5,12 @@ var expect = require('chai').expect;
 describe('Math Labs', function() {
 //addition sum() wrapper for further organization
 describe('#sum()', function() {
-
-  //Opted to comment test out to make solution more beginner friendly
-  // context('without arguments', function() {
-  //   it('should return 0', function() {
-  //     expect(sum()).to.equal(0)
-  //   })
-  // })
-  
   context('with number arguments', function() {
     it('should return sum of arguments', function() {
-      //Modified test for simpler solution - it is a beginners tutorial after all
-      //was expect(sum(1, 2, 3, 4, 5)).to.equal(15)
       expect(sum(1, 2)).to.equal(3)
+      })
     })
-    
-  //   it('should return argument when only one argument is passed', function() {
-  //     expect(sum(5)).to.equal(5)
-  //   })
-  // })
-  
-  //Opted to comment test out to make solution more beginner friendly
-  // context('with non-number arguments', function() {
-  //   it('should throw error', function() {
-  //     expect(function() {
-  //       sum(1, 2, '3', [4], 5)
-  //     }).to.throw(TypeError, 'sum() expects only numbers.')
-  //   })
-  // })
-})
-})
-//Unique test-casing, based on above example
+  })
 //sub() wrapper
 describe('#sub()', function() {
     describe('with number arguments', function() {
@@ -46,7 +18,7 @@ describe('#sub()', function() {
             expect(sub(1, 2)).to.equal(-1)
         })
     })
-})
+  })
 })
 
 //A second class wrapper for Non-Math Labs
